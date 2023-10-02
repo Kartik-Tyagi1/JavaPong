ab6a4c296c434a703fc7f0f6d9103a821609fe87
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2023-10-02 10:08:43 -0400

    Added key listener class to bind key board events

--- src/Window.java
+++ src/Window.java
@@ -1,25 +1,33 @@
 import javax.swing.JFrame;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable{
-    public int x = 20;
-    public int y = 20;
     Graphics2D g2;
+    KL keyListener = new KL();
+
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
         this.setTitle(Constants.title);
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes when we click x on the window
+
         g2 = (Graphics2D)this.getGraphics();
+        this.addKeyListener(keyListener);
     }
 
     public void update(double deltaTime) {
         g2.setColor(Color.BLACK);
-        g2.fillRect(x, y, 50, 50);
-        x += 2;
-        y += 2;
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+        if(keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+            System.out.println("User pressed up arrow");
+        }
+        else if(keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+            System.out.println("User pressed down arrow");
+        }
     }
 
     @Override
